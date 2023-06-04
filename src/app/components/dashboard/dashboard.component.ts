9b015794f82d250918026f102257dad6999370ad
Keerthana <mailtokeerthu62@gmail.com>
2023-06-04 16:20:15 +0530

    search

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Subject } from 'rxjs';
 import { MovieService } from 'src/app/services/movie.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -13,23 +15,59 @@ export class DashboardComponent implements OnInit {
   public username:string="";
   public islogin:boolean=false;
 
-  constructor(private user:UserService,private movie:MovieService) { }
+  constructor(private user:UserService,
+    private movie:MovieService,
+    private toastrService: ToastrService,
+    private elementRef:ElementRef) { }
 
   ngOnInit(): void {
     if(this.user.isLoggedIn())
     {
       this.islogin=true;
       this.getUserName();
-      this.movie.getMovies().subscribe(res=>{
+      this.loadMovies();
+    }
+    
+  }
+
+  searchMoviesByName(event:any) {
+    const searchText=event.target.value;
+    //id="search-clear"
+    if(searchText=="")
+    {
+      this.loadMovies();
+    }
+    else
+    {
+      this.movie.getMoviesByName(searchText).subscribe({
+        next:(res)=>{
+          console.log(res);
+           this.movies = res;
+          this.toastrService.success('Movie Details',res.message, {
+            timeOut: 3000,
+          });
+        },
+        error:(err)=>{
+          this.toastrService.error('Something went wrong',err.message, {
+            timeOut: 3000,
+          });
+          console.log(err);
+        }
+      });
+    }
+    // this.movie.getMoviesByName(searchStr).subscribe(res => {
+    //   console.log(res);
+    //   this.movies = res;
+    // });
+    
+  }
+
+  loadMovies() {
+    this.movie.getMovies().subscribe(res=>{
       this.movies=res;
       console.log(this.movies);
      });
-    }
   }
-
-  // getAllMovies(){
-
-  // }
 
   getUserName(){
     this.username=this.user.getUserName();
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,16 @@ import { Injectable } from '@angular/core';
 export class MovieService {
 
   private baseUrl:string="https://localhost:7108/movie/api/v1.0/moviebooking";
+  //https://localhost:7108/movie/api/v1.0/moviebooking/movies/search/moviename?moviename=Avatar
 
   constructor(private http:HttpClient) { }
 
   getMovies(){
     return this.http.get<any>(`${this.baseUrl}/all`);
+  }
+
+  getMoviesByName(searchString:string){
+    let queryParams = new HttpParams().append("moviename",searchString);
+    return this.http.get<any>(`${this.baseUrl}/movies/search/moviename`,{params:queryParams});
   }
 }
